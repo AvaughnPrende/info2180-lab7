5a915002c75cf8749887aff946a4097ea09e0a26
Avaughn Prendergast <avaughn.prendergast@mymona.uwi.edu>
2017-11-15 17:15:19 +0000

    COMPLETED ALL TASKS

--- world.js
+++ world.js
@@ -14,13 +14,13 @@ $(document).ready(function(){
     searchButton.on("click", function(element){
         element.preventDefault();
         
-        searchTerm  = getSearchTerm().toLocaleLowerCase().trim();
+        searchTerm  = getSearchTerm();
         
         if (checkbox.prop("checked")){
-            url         = 'https://info2180-lab7-avaughnprende.c9users.io/world1.php?all=true';
+            url         = 'https://info2180lab-7-avaughnprende.c9users.io/world.php/?all=true';
         }
         else{
-            url         = 'https://info2180-lab7-avaughnprende.c9users.io/world1.php?country=' + searchTerm;
+            url         = 'https://info2180lab-7-avaughnprende.c9users.io/world.php/?country=' + searchTerm.toLocaleLowerCase().trim();
         }
         
         console.log(searchTerm);
@@ -36,17 +36,24 @@ $(document).ready(function(){
         if (httpRequest.readyState === httpRequest.DONE){
             if(httpRequest.status  === 200){
                 if(searchResultFound(httpRequest.responseText)){
+                    resultDiv.empty();
                      insertText(httpRequest.responseText);
                 }
                 else{
-                    insertText(`"${searchTerm}" NOT FOUND!`);
+                    insertText('0 Results found');
                 }
             }
         }
     }
     
     function insertText(text){
-        resultDiv.html(text);
+        if(url.endsWith("all=true")){
+            resultDiv.html("<h3>All Countries Listed</h3>")
+        }
+        else{
+            resultDiv.html(`<h3>Search results found for "${searchTerm}"` );
+        }
+        resultDiv.append(text);
     }
     
     function searchResultFound(searchResponse){
--- world.php
+++ world.php
@@ -12,7 +12,7 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo $country;
+//echo $country;
 echo '<ul>';
 foreach ($results as $row) {
   echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
